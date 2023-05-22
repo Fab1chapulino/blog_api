df6a67f71477141db25460c59390ca065ec21634
Fabian Romero <chapulinozeta@gmail.com>
2023-05-21 20:39:09 -0400

    Defined database provider and constants

---
+++ src/core/constants/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export const SEQUELIZE = 'SEQUELIZE';
+export const DEVELOPMENT = 'development';
+export const TEST = 'test';
+export const PRODUCTION = 'production';
