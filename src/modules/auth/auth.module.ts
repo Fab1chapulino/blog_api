78536d2d11e7d232f4513627327f3da785194f1c
Fabian Romero <chapulinozeta@gmail.com>
2023-05-22 11:42:08 -0400

    Added modules and provider to auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [PassportModule, UsersModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
