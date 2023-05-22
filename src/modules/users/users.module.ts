e12085e5d0d2b1b0ffbd953467ce656edfc294d5
Fabian Romero <chapulinozeta@gmail.com>
2023-05-22 10:50:40 -0400

    Added provider to users module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { usersProviders } from './users.provider';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService, ...usersProviders],
 })
 export class UsersModule {}
